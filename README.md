6af81068f2aa3f5a248be4dbd8f011a4911326f1
Zeinok <zeinok@protonmail.com>
2020-06-29 15:08:45 +0800

    Add readme.md on running, minimum sample size

---
+++ README.md
@@ -0,0 +1,16 @@
+# MotionGesture
+
+## Install
+```
+pip3 install --user poetry
+poetry install
+```
+## Run
+```
+poetry run motiongst [args]
+```
+
+Example: Run with DEBUG
+```
+poetry run motiongst -l 0
+```
--- motion_gesture/motion_gesture.py
+++ motion_gesture/motion_gesture.py
@@ -2,16 +2,18 @@
 import argparse
 import time
 import logging
+from os.path import dirname as path_dirname
 from os import name as os_name
 
 from cv2 import cv2
 import imutils
 
-from direction import Direction
+import motion_gesture
+from motion_gesture.direction import Direction
 if os_name == "nt":
-    from nt_window_action import WindowAction
+    from motion_gesture.nt_window_action import WindowAction
 else:
-    from kde_window_action import WindowAction
+    from motion_gesture.kde_window_action import WindowAction
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--min-area", type=int, default=9000, help="minimum area size")
@@ -22,119 +24,120 @@ ap.add_argument("-d", "--dilation", type=int, default=2, help="dilation")
 ap.add_argument("-f", "--file", type=str, default=0, help="file")
 ap.add_argument("-w", "--wait", type=int, default=300, help="ms to wait")
 ap.add_argument("-l", "--log", type=int, default=20, help="log level")
+ap.add_argument("-m", "--min-sample", type=int, default=5, help="minimum sample")
 args = vars(ap.parse_args())
 camera = cv2.VideoCapture(args["file"])
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
 logging.basicConfig(level=args["log"])
 
-previousFrame = None
+logging.info("Loading {}".format(path_dirname(motion_gesture.__file__) + "haarcascade_frontalface_default.xml"))
+face_cascade = cv2.CascadeClassifier(path_dirname(motion_gesture.__file__) + "/haarcascade_frontalface_default.xml")
 
-previous_x, previous_y = -1, -1
-previous_dx, previous_dy = 0, 0
-largest_area_x = 0
-largest_area_y = 0
+def main():
+    previousFrame = None
+    previous_x, previous_y = -1, -1
+    previous_dx, previous_dy = 0, 0
+    largest_area_x = 0
+    largest_area_y = 0
 
-direction_log = []
-last_movement = time.time() * 1000
+    direction_log = []
+    last_movement = time.time() * 1000
+    while True:
 
-while True:
+        grabbed, frame = camera.read()
+        frame = imutils.resize(frame, width=500)
+        frame = cv2.flip(frame, 1)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 3, 0)
+        gray = cv2.GaussianBlur(gray, (args["blur"], args["blur"]), 0)
+        # if the first frame is None, initialize it
+        if previousFrame is None:
+            previousFrame = gray
+            continue
+        # compute the absolute difference between the current frame and
+        # first frame
+        frameDelta = cv2.absdiff(previousFrame, gray)
+        thresh = cv2.threshold(frameDelta, args["threshold"], 255, cv2.THRESH_BINARY)[1]
+        # dilate the thresholded image to fill in holes, then find contours
+        # on thresholded image
+        thresh = cv2.dilate(thresh, None, iterations=args["dilation"])
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
 
-    grabbed, frame = camera.read()
-    frame = imutils.resize(frame, width=500)
-    frame = cv2.flip(frame, 1)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 3, 0)
-    gray = cv2.GaussianBlur(gray, (args["blur"], args["blur"]), 0)
-    # if the first frame is None, initialize it
-    if previousFrame is None:
         previousFrame = gray
-        continue
-    # compute the absolute difference between the current frame and
-    # first frame
-    frameDelta = cv2.absdiff(previousFrame, gray)
-    thresh = cv2.threshold(frameDelta, args["threshold"], 255, cv2.THRESH_BINARY)[1]
-    # dilate the thresholded image to fill in holes, then find contours
-    # on thresholded image
-    thresh = cv2.dilate(thresh, None, iterations=args["dilation"])
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
+        frameDelta = cv2.cvtColor(frameDelta, cv2.COLOR_GRAY2RGB)
+        # loop over the contours
+        largest_area = 0
+        largest_area_x, largest_area_y = -1, -1
+        for c in cnts:
+            # if the contour is too small, ignore it
+            area = cv2.contourArea(c)
+            if area < args["min_area"]:
+                continue
+            if area > args["max_area"]:
+                continue
 
-    previousFrame = gray
-    frameDelta = cv2.cvtColor(frameDelta, cv2.COLOR_GRAY2RGB)
-    # loop over the contours
-    largest_area = 0
-    largest_area_x, largest_area_y = -1, -1
-    for c in cnts:
-        # if the contour is too small, ignore it
-        area = cv2.contourArea(c)
-        if area < args["min_area"]:
-            continue
-        if area > args["max_area"]:
-            continue
+            # compute the bounding box for the contour, draw it on the frame,
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(
+                frame,
+                str(area),
+                (x, y),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (0, 255, 0),
+                2,
+            )
+            if area > largest_area:
+                largest_area_x, largest_area_y = x, y
+                largest_area = area
+            last_movement = time.time() * 1000
 
-        # compute the bounding box for the contour, draw it on the frame,
-        (x, y, w, h) = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(
-            frame,
-            str(area),
-            (x, y),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.5,
-            (0, 255, 0),
-            2,
-        )
-        if area > largest_area:
-            largest_area_x, largest_area_y = x, y
-            largest_area = area
-        last_movement = time.time() * 1000
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.imshow("Frame", frame)
+        cv2.imshow("Frame Delta", frameDelta)
+        cv2.imshow("Thresh", thresh)
+        
+        
+        dx, dy = largest_area_x - previous_x, largest_area_y - previous_y
+        previous_dx, previous_dy = dx, dy
+        previous_x, previous_y = largest_area_x, largest_area_y
+        
+        dx, dy = previous_dx*0.3 + dx*0.7, previous_dy*0.5 + dy*0.7
+        # dx, dy = dx/2, dy/2
+        dx, dy = int(dx), int(dy)
+        
 
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    cv2.imshow("Frame", frame)
-    cv2.imshow("Frame Delta", frameDelta)
-    cv2.imshow("Thresh", thresh)
-    
-    
-    dx, dy = largest_area_x - previous_x, largest_area_y - previous_y
-    previous_dx, previous_dy = dx, dy
-    previous_x, previous_y = largest_area_x, largest_area_y
-    
-    dx, dy = previous_dx*0.3 + dx*0.7, previous_dy*0.5 + dy*0.7
-    # dx, dy = dx/2, dy/2
-    dx, dy = int(dx), int(dy)
-    
-
-    if dx!=0 or dy!=0:
-        direction = Direction.Null
-        if abs(dx)>abs(dy):
-            direction = Direction.Left if dx < 0 else Direction.Right
-        else:
-            direction = Direction.Up if dy < 0 else Direction.Down
-        logging.debug("{}, dx={}, dy={}".format(direction, dx, dy))
-        direction_log.append(direction)
-    
-    if time.time() * 1000 - last_movement > args["wait"]:
-        if len(direction_log) > 0:
-            direction_result = max(direction_log, key=direction_log.count)
-            logging.info("Result: {}".format(direction_result))
-            if direction_result==Direction.Right:
-                WindowAction.previousDesktop()
-            if direction_result==Direction.Left:
-                WindowAction.nextDesktop()
-            if direction_result==Direction.Up:
-                WindowAction.maximizeWindow()
-            if direction_result==Direction.Down:
-                WindowAction.minimizeWindow()
-            # for e in Direction:
-            #     logging.info("Result: {} {}x".format(e, direction_log.count(e)))
+        if dx!=0 or dy!=0:
+            direction = Direction.Null
+            if abs(dx)>abs(dy):
+                direction = Direction.Left if dx < 0 else Direction.Right
+            else:
+                direction = Direction.Up if dy < 0 else Direction.Down
+            logging.debug("{}, dx={}, dy={}".format(direction, dx, dy))
+            direction_log.append(direction)
+        
+        if time.time() * 1000 - last_movement > args["wait"]:
+            if len(direction_log) > args["min_sample"]:
+                direction_result = max(direction_log, key=direction_log.count)
+                logging.info("Result: {}".format(direction_result))
+                if direction_result==Direction.Right:
+                    WindowAction.previousDesktop()
+                if direction_result==Direction.Left:
+                    WindowAction.nextDesktop()
+                if direction_result==Direction.Up:
+                    WindowAction.maximizeWindow()
+                if direction_result==Direction.Down:
+                    WindowAction.minimizeWindow()
+                # for e in Direction:
+                #     logging.info("Result: {} {}x".format(e, direction_log.count(e)))
             direction_log.clear()
 
-    k = cv2.waitKey(10) & 0xFF
-    if k == 27:
-        break
-
-
-camera.release()
-cv2.destroyAllWindows()
+        k = cv2.waitKey(10) & 0xFF
+        if k == 27:
+            break
+    camera.release()
+    cv2.destroyAllWindows()
 
